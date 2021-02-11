cb41468175df356a399e10496c02946e58b38b3a
0kkun <shinji.okumoto.01@gmail.com>
2021-02-11 21:58:28 +0900

    add: layout用ファイル追加

---
+++ server/resources/views/common/header.blade.php
@@ -0,0 +1,20 @@
+<div class="p-3 bg-color-brown" id="header-view">
+    <div class="d-flex">
+        <div class="h3">
+            <i class="fas fa-paw pr-2 pl-2 text-white"></i>
+            <a class="text-white font-coda-caption" style="text-decoration:none;" href="{{ route('home.index') }}">Matching Service</a>
+        </div>
+    </div>
+</div>
+
+<style>
+.bg-color-brown {
+    background-color:#18bc9c;
+}
+.font-coda-caption {
+    font-family: 'Coda Caption', sans-serif;
+}
+.a-decorate-none {
+    text-decoration:none;
+}
+</style>
