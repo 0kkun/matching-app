22b01fe6a639e8bd6129bcf0b51f32b027711a22
0kkun <shinji.okumoto.01@gmail.com>
2021-02-11 23:54:48 +0900

    add: sidebar仮実装

---
+++ server/resources/js/components/SideBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card">
+        <ul>
+            <li class="side-bar-btn"><RouterLink to="/home">Search</RouterLink></li>
+            <li class="side-bar-btn">Comunity</li>
+            <li class="side-bar-btn">Like</li>
+            <li class="side-bar-btn">Message</li>
+            <li class="side-bar-btn"><RouterLink to="/home/profile">Profile</RouterLink></li>
+            <li class="side-bar-btn">Etc</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.side-bar-btn {
+    list-style: none;
+    margin:  0;
+    padding: 0;
+}
+</style>
--- server/resources/js/pages/Search.vue
+++ server/resources/js/pages/Search.vue
@@ -1,24 +1,41 @@
 <template>
     <div class="container">
-        <div class="card m-3 p-3">
-            <div class="input-group pr-5 pl-5">
-                <input type="text" class="form-control" placeholder="Please input keywords..." aria-label="Please input keywords..." aria-describedby="button-addon">
-                <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="button">
-                        <i class="fas fa-search"></i>
-                    </button>
+        <div class="row">
+
+            <div class="col-sm-3 pt-3">
+                    <SideBar />
+            </div>
+
+            <div class="col-sm-9">
+                <div class="card m-3 p-3">
+                    <div class="input-group pr-5 pl-5">
+                        <input type="text" class="form-control" placeholder="Please input keywords..." aria-label="Please input keywords..." aria-describedby="button-addon">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </div>
     </div>
 </template>
 
+
+
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+import SideBar from '../components/SideBar.vue'
+
+export default {
+    components: {
+        SideBar,
+    },
+    mounted() {
+        console.log('Component mounted.')
     }
+}
 </script>
 
 <style lang="scss" scoped>
