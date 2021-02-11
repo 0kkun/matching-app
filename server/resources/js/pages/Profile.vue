d16f58bbe2a130cae083128c74bd631f8af54037
0kkun <shinji.okumoto.01@gmail.com>
2021-02-11 23:54:12 +0900

    add: profile仮実装

---
+++ server/resources/js/pages/Profile.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-3 pt-3">
+                <SideBar />
+            </div>
+
+            <div class="col-sm-9">
+                <div>ゲストさん</div>
+                <div>画像</div>
+                <div>一言 : </div>
+                <div>自己紹介</div>
+                <div>年齢 : </div>
+                <div>趣味 : </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import SideBar from '../components/SideBar.vue'
+
+export default {
+    components: {
+        SideBar,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
