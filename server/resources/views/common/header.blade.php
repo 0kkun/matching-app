dd31a1d079307733334268ce0a2c6abdbba4bddc
0kkun <shinji.okumoto.01@gmail.com>
2021-02-11 23:13:18 +0900

    update: ヘッダーのアイコン変更

--- server/resources/views/common/header.blade.php
+++ server/resources/views/common/header.blade.php
@@ -1,7 +1,7 @@
 <div class="p-3 bg-color-blue-std" id="header-view">
     <div class="d-flex justify-content-between">
         <div class="h3">
-            <i class="fas fa-paw pr-2 pl-2 text-white"></i>
+            <i class="fas fa-asterisk pr-2 pl-2 text-white"></i>
             <a class="text-white font-coda-caption" style="text-decoration: none;" href="{{ route('home.index') }}">Matching Service</a>
         </div>
         <div>
