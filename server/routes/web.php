a2309b3c3adf6f8d648866456ac010745c7f460d
0kkun <shinji.okumoto.01@gmail.com>
2021-02-11 21:58:48 +0900

    add: homeのroute追加

--- server/routes/web.php
+++ server/routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home.index');
+
+Route::get('/home/{any?}', function() {
+    return view('home.index');
+})->where('any', '.*');
