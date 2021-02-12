a18088b6d1e52f969d486ac1b7b7a6e57c871e83
0kkun <shinji.okumoto.01@gmail.com>
2021-02-12 22:46:57 +0900

    update: プロフィール画面修正

--- server/resources/js/pages/Profile.vue
+++ server/resources/js/pages/Profile.vue
@@ -7,12 +7,16 @@
             </div>
 
             <div class="col-sm-9">
-                <div>ゲストさん</div>
-                <div>画像</div>
-                <div>一言 : </div>
-                <div>自己紹介</div>
-                <div>年齢 : </div>
-                <div>趣味 : </div>
+                <div class="card mt-3">
+                    <img class="mx-auto mt-3" style="width:500px; height:350px" src="/images/cat1.jpeg">
+                    <div class="h3 text-center pt-2">ゲストさん</div>
+                    <div class="h4 text-center pt-2">一言 : 今日もいい天気</div>
+                    <div class="h4 text-center text-primary">--自己紹介--</div>
+                    <div class="h5 text-center p-3 ml-3 mr-3 bg-light">どうもです。テニス仲間募集中</div>
+                    <div class="h5 text-center">年齢 : 25</div>
+                    <div class="h5 text-center">趣味 : 野球</div>
+                    <div class="h5 text-center">住所 : 東京</div>
+                </div>
             </div>
 
         </div>
