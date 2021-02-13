29cf4c9a1efe40cf8c0a3798355d823b5a53c55e
0kkun <shinji.okumoto.01@gmail.com>
2021-02-13 12:10:35 +0900

    add: 新規登録時のフォーム編集

--- server/resources/views/auth/register.blade.php
+++ server/resources/views/auth/register.blade.php
@@ -40,6 +40,53 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="prefecture_id" class="col-md-4 col-form-label text-md-right">{{ __('Prefecture') }}</label>
+                            <div class="col-md-6">
+                                <select class="w-50 form-control @error('prefecture_id') is-invalid @enderror" name="prefecture_id" id="prefecture_id">
+                                    <option value="">---</option>
+                                    @for ($i=1; $i<count(Config::get('pref')); $i++)
+                                        <option value="{{ $i }}">{{ Config::get('pref')[$i] }}</option>
+                                    @endfor
+                                </select>
+
+                                @error('prefecture_id')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="birthday" class="col-md-4 col-form-label text-md-right">{{ __('Birthday') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="birthday" type="date" class="w-50 form-control @error('birthday') is-invalid @enderror" name="birthday" value="{{ old('birthday') }}" required autocomplete="birthday">
+
+                                @error('birthday')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="sex" class="col-md-4 col-form-label text-md-right">{{ __('Sex') }}</label>
+
+                            <div class="col-md-6 pt-2">
+                                <label for="sex">男性 <input class="@error('sex') is-invalid @enderror" id="r_male" type="radio" name="sex" value="1"></label>
+                                <label for="sex">女性 <input class="@error('sex') is-invalid @enderror" id="r_female" type="radio" name="sex" value="2"></label>
+
+                                @error('sex')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
