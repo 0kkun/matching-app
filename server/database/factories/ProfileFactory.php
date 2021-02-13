5bcbf5441397e9dec2e2e27509e717519f7e0cac
0kkun <shinji.okumoto.01@gmail.com>
2021-02-13 18:32:21 +0900

    add: ProfileFactory作成

---
+++ server/database/factories/ProfileFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\User;
+use App\Models\Profile;
+use Faker\Generator as Faker;
+
+$factory->define(Profile::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->randumNumber(3), // 3桁のランダム数値
+        'tweet' => $faker->realText(50),
+        'introduction' => $faker->realText($maxNbChars = 100,$indexSize = 3),
+        'hobby' => $faker->text(10),
+        'blood_type' => 'B',
+        'job' => $faker->jobTitle()
+    ];
+});
--- server/database/factories/UserFactory.php
+++ server/database/factories/UserFactory.php
@@ -23,7 +23,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'prefecture_id' => $faker->numberBetween($min=1, $max=47),
-        'birthday' => $faker->date(),
+        'birthday' => $faker->dateTimeBetween('-40 years', '-20years')->format('Y-m-d'), // 20歳~40歳
         'sex' => $faker->numberBetween($min=1, $max=2),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
