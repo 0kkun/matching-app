e59e8d014f5098820aa16e7343aa34026bccf66b
0kkun <shinji.okumoto.01@gmail.com>
2021-02-13 12:39:13 +0900

    add: Userリポジトリメソッド実装

--- server/app/Repositories/Contracts/UserRepository.php
+++ server/app/Repositories/Contracts/UserRepository.php
@@ -2,6 +2,22 @@
 
 namespace App\Repositories\Contracts;
 
+use Illuminate\Support\Collection;
+
 interface UserRepository
 {
+    /**
+     * 全てのユーザーを取得する
+     *
+     * @return Collection
+     */
+    public function fetchAll(): Collection;
+
+    /**
+     * idでユーザー情報を取得する
+     *
+     * @param integer $user_id
+     * @return Collection
+     */
+    public function getByUserId(int $user_id): Collection;
 }
--- server/app/Repositories/Eloquents/EloquentUserRepository.php
+++ server/app/Repositories/Eloquents/EloquentUserRepository.php
@@ -3,7 +3,43 @@
 namespace App\Repositories\Eloquents;
 
 use App\Repositories\Contracts\UserRepository;
+use Illuminate\Support\Collection;
+use App\Models\User;
 
 class EloquentUserRepository implements UserRepository
 {
+    protected $users;
+
+    /**
+    * @param object $users
+    */
+    public function __construct(
+        User $users
+    )
+    {
+        $this->users = $users;
+    }
+
+
+    /**
+     * 全てのユーザーを取得する
+     *
+     * @return Collection
+     */
+    public function fetchAll(): Collection
+    {
+        return $this->users->get();
+    }
+
+
+    /**
+     * idでユーザー情報を取得する
+     *
+     * @param integer $user_id
+     * @return Collection
+     */
+    public function getByUserId(int $user_id): Collection
+    {
+        return $this->users->where('id', $user_id)->get();
+    }
 }
