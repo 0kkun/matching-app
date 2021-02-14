dd7d1a7767d90549b51238a847970939e6007c6f
0kkun <shinji.okumoto.01@gmail.com>
2021-02-14 10:31:35 +0900

    add: profileリポジトリメソッド追加

--- server/app/Repositories/Contracts/ProfileRepository.php
+++ server/app/Repositories/Contracts/ProfileRepository.php
@@ -2,6 +2,31 @@
 
 namespace App\Repositories\Contracts;
 
+use Illuminate\Support\Collection;
+
 interface ProfileRepository
 {
+    /**
+     * user_idでプロフィールデータを取得する
+     *
+     * @param integer $user_id
+     * @return Collection
+     */
+    public function getProfile(int $user_id): Collection;
+
+    /**
+     * 引数で指定されたユーザーのプロフィールが存在するかどうか
+     *
+     * @param integer $user_id
+     * @return boolean
+     */
+    public function isExistsProfile(int $user_id): bool;
+
+    /**
+     * 初期のデフォルトプロフィールを作成
+     *
+     * @param integer $user_id
+     * @return void
+     */
+    public function createDefaultProfile(int $user_id): void;
 }
--- server/app/Repositories/Eloquents/EloquentProfileRepository.php
+++ server/app/Repositories/Eloquents/EloquentProfileRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Eloquents;
 
 use App\Models\Profile;
 use App\Repositories\Contracts\ProfileRepository;
+use Illuminate\Support\Collection;
 
 class EloquentProfileRepository implements ProfileRepository
 {
@@ -17,5 +18,42 @@ class EloquentProfileRepository implements ProfileRepository
     )
     {
         $this->profiles = $profiles;
+    }
+
+    /**
+     * user_idでプロフィールデータを取得する
+     *
+     * @param integer $user_id
+     * @return Collection
+     */
+    public function getProfile(int $user_id): Collection
+    {
+        return $this->profiles
+            ->where('user_id', $user_id)
+            ->get();
+    }
+
+    /**
+     * 引数で指定されたユーザーのプロフィールが存在するかどうか
+     *
+     * @param integer $user_id
+     * @return boolean
+     */
+    public function isExistsProfile(int $user_id): bool
+    {
+        return $this->profiles
+            ->where('user_id', $user_id)
+            ->exists();
+    }
+
+    /**
+     * 初期のデフォルトプロフィールを作成
+     *
+     * @param integer $user_id
+     * @return void
+     */
+    public function createDefaultProfile(int $user_id): void
+    {
+        $this->profiles->create(['user_id' => $user_id]);
     }
 }
