c6bfb4dbf419cfb76b85e8bbf5b7873780182f18
0kkun <shinji.okumoto.01@gmail.com>
2021-02-16 00:37:57 +0900

    add: ServiceProviderの作成と登録

---
+++ server/app/Providers/MyServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MyServiceProvider extends ServiceProvider
+{
+    /**
+     * サービス名を配列にまとめる
+     *
+     * @var array
+     */
+    public $services = [
+        'Profile',
+    ];
+
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // サービス名でBindする
+        // MEMO: まとめてbindする際、\は2本入れとかないとエラーが出る
+        foreach ($this->services as $service) {
+            $this->app->bind(
+                "App\\Services\\{$service}\\{$service}ServiceInterface",
+                "App\\Services\\{$service}\\{$service}Service"
+            );
+        }
+    }
+}
