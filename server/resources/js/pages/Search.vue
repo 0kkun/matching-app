da0e4f813d8022d606a36a99494ba47a8b1cb10d
0kkun <shinji.okumoto.01@gmail.com>
2021-02-16 13:29:55 +0900

    add: テキスト検索機能実装

--- server/resources/js/pages/Search.vue
+++ server/resources/js/pages/Search.vue
@@ -7,75 +7,72 @@
 
             <div class="col-sm-9">
                 <div class="card mt-3 mb-3 pt-3 pb-3">
-                    <form action="">
-                        <div class="input-group pr-5 pl-5">
-                            <input type="text" class="form-control" placeholder="Please input keywords..." aria-label="Please input keywords...">
-                        </div>
+                    <div class="input-group pr-5 pl-5">
+                        <input v-model="keywords" type="text" class="form-control" placeholder="Please input keywords..." aria-label="Please input keywords...">
+                    </div>
 
-                        <div class="row text-right">
-                            <div class="col-6">
-                                <div class="pt-3 pl-3">
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="sex" value="1" v-model="checkSex">
-                                        <label class="form-check-label" for="sex">男</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="sex" value="2" v-model="checkSex">
-                                        <label class="form-check-label" for="sex">女</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="sex" value="" v-model="checkSex" checked>
-                                        <label class="form-check-label" for="sex">指定無し</label>
-                                    </div>
+                    <div class="row text-right">
+                        <div class="col-6">
+                            <div class="pt-3 pl-3">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="sex" value="1" v-model="checkSex">
+                                    <label class="form-check-label" for="sex">男</label>
                                 </div>
-                                
-                                <div class="pt-2  pl-3">
-                                    <label>血液型：</label>
-                                    <select name="blood_type" v-model="checkBloodType">
-                                        <option value="">指定無し</option>
-                                        <option value="A">A</option>
-                                        <option value="B">B</option>
-                                        <option value="AB">AB</option>
-                                        <option value="O">O</option>
-                                    </select>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="sex" value="2" v-model="checkSex">
+                                    <label class="form-check-label" for="sex">女</label>
                                 </div>
-                                <div class="pt-2  pl-3">
-                                    <label>住所：</label>
-                                    <select name="prefecture_id" v-model="checkPref">
-                                        <option value="">指定無し</option>
-                                        <option v-for="(pref, index) in prefLists" :key="pref.id" :value="index">{{ pref }}</option>
-                                    </select>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="sex" value="" v-model="checkSex" checked>
+                                    <label class="form-check-label" for="sex">指定無し</label>
                                 </div>
                             </div>
-                            <div class="col-6 text-left">
-                                <div class="pt-3 pl-3">
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" name="image_setting" v-model="checkImageSetting">
-                                        <label class="form-check-label" for="image_setting">画像設定済み</label>
-                                    </div>
+                            
+                            <div class="pt-2  pl-3">
+                                <label>血液型：</label>
+                                <select name="blood_type" v-model="checkBloodType">
+                                    <option value="">指定無し</option>
+                                    <option value="A">A</option>
+                                    <option value="B">B</option>
+                                    <option value="AB">AB</option>
+                                    <option value="O">O</option>
+                                </select>
+                            </div>
+                            <div class="pt-2  pl-3">
+                                <label>住所：</label>
+                                <select name="prefecture_id" v-model="checkPref">
+                                    <option value="">指定無し</option>
+                                    <option v-for="(pref, index) in prefLists" :key="pref.id" :value="index">{{ pref }}</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-6 text-left">
+                            <div class="pt-3 pl-3">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" name="image_setting" v-model="checkImageSetting">
+                                    <label class="form-check-label" for="image_setting">画像設定済み</label>
                                 </div>
-                                <div class="pt-2 pl-3">
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" name="good" value="1">
-                                        <label class="form-check-label" for="sex">いいねが多い順</label>
-                                    </div>
+                            </div>
+                            <div class="pt-2 pl-3">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" name="good" value="1">
+                                    <label class="form-check-label" for="sex">いいねが多い順</label>
                                 </div>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="text-right pr-4">
-                            <button @click="reset()" href="#" class="btn btn-danger mr-2">Reset</button>
-                            <button @click="search()" class="btn btn-success" type="button">
-                                <i class="fas fa-search"></i>
-                            </button>
-                        </div>
-
-                    </form>
+                    <div class="text-right pr-4">
+                        <button @click="reset()" href="#" class="btn btn-danger mr-2">Reset</button>
+                        <button @click="search()" class="btn btn-success" type="button">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </div>
                 </div>
 
-                <div class="card-columns pl-2">
-                    <div v-if="searchMode">
-                        <div class="w-100">検索中</div>
+                <div v-if="searchMode">
+                    <div class="text-center">検索中</div>
+                    <div class="card-columns pl-2">
                         <div v-for="user in searchUsers" :key="user.id">
                             <div class="card" style="height:290px; width:250px;">
                                 <div v-if="user.image_name=='no_image.png'">
@@ -96,8 +93,12 @@
                             </div>
                         </div>
                     </div>
-                    <div v-else>
-                        <div class="w-100">全件表示中</div>
+                </div>
+
+
+                <div v-else>
+                    <div class="text-center">全件表示中</div>
+                    <div class="card-columns pl-2">
                         <div v-for="user in users" :key="user.id">
                             <div class="card" style="height:290px; width:250px;">
                                 <div v-if="user.image_name=='no_image.png'">
@@ -145,6 +146,7 @@ export default {
             checkImageSetting: '',
             checkBloodType: '',
             checkPref: '',
+            keywords: '',
         }
     },
     mounted() {
@@ -157,7 +159,6 @@ export default {
                 this.users = response.data.data.users;
                 this.prefLists = response.data.data.pref_lists;
                 this.loadStatus = true;
-                console.log(this.prefLists);
             })
             .catch((error) => {
                 console.log(error);
@@ -191,8 +192,14 @@ export default {
                     return user.prefecture_id == this.checkPref;
                 },this);
             }
+            // キーワード検索
+            if (this.keywords != '') {
+                this.searchUsers = this.searchUsers.filter((user) => {
+                    return user.introduction.indexOf(this.keywords) != -1
+                },this);
+            }
             // 何もチェックされていなければ検索モードを解除
-            if (this.checkImageSetting=='' && this.checkSex=='' && this.checkBloodType=='' && this.checkPref=='') {
+            if (this.checkImageSetting=='' && this.checkSex=='' && this.checkBloodType=='' && this.checkPref=='' && this.keywords=='') {
                 this.searchUsers = '';
                 this.searchMode = false;
             }
@@ -203,6 +210,8 @@ export default {
             this.checkImageSetting = '';
             this.checkBloodType = '';
             this.checkPref = '';
+            this.keywords = '';
+            this.searchMode = false;
         }
     },
 }
