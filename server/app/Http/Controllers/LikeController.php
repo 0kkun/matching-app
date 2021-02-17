b7e4630a98bd03f32de622199df3c516066c6352
0kkun <shinji.okumoto.01@gmail.com>
2021-02-17 11:50:09 +0900

    add: いいねボタン押下時のロジック実装

--- server/app/Http/Controllers/LikeController.php
+++ server/app/Http/Controllers/LikeController.php
@@ -4,16 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Repositories\Contracts\LikeRepository;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class LikeController extends Controller
 {
+    private $like_repository;
     /**
      * Constructor
      *
      */
     public function __construct(
+        LikeRepository $like_repository
     )
     {
+        $this->like_repository = $like_repository;
     }
 
 
@@ -26,9 +32,28 @@ class LikeController extends Controller
     public function createLikeRequest(Request $request)
     {
         try {
-
+            Log::info("[START] " . __FUNCTION__ );
+            if ($request->has('receive_user_id')) {
+                $request_user_id = Auth::id();
+                $receive_user_id = $request->receive_user_id;
+                $this->like_repository->createLikeRequest($request_user_id, $receive_user_id);
+                $status = 201;
+            } else {
+                $status = 400;
+            }
+            $response = [
+                'status'  => $status,
+                'message' => '',
+            ];
+            Log::info("[ END ] " . __FUNCTION__ . ", STATUS:" . $status);
         } catch (\Exception $e) {
-
+            Log::info("[Exception]" . __FUNCTION__ . $e->getMessage());
+            $status = 500;
+            $response = [
+                'status' => $status,
+                'message' => $e->getMessage(),
+            ];
         }
+        return response()->json($response);
     }
 }
