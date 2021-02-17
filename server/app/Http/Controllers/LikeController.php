c759463f85716709449dcc9d712125b9047bd16f
0kkun <shinji.okumoto.01@gmail.com>
2021-02-17 11:10:38 +0900

    add: LikeController作成

---
+++ server/app/Http/Controllers/LikeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class LikeController extends Controller
+{
+    /**
+     * Constructor
+     *
+     */
+    public function __construct(
+    )
+    {
+    }
+
+
+    /**
+     * いいねボタン押下時のアクション
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function createLikeRequest(Request $request)
+    {
+        try {
+
+        } catch (\Exception $e) {
+
+        }
+    }
+}
