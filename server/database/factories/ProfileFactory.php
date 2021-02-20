5e8f7504632ec326cb11f955c578b3cf105844ce
0kkun <shinji.okumoto.01@gmail.com>
2021-02-21 02:02:21 +0900

    fix: インデント修正

--- server/database/factories/ProfileFactory.php
+++ server/database/factories/ProfileFactory.php
@@ -2,7 +2,6 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Models\User;
 use App\Models\Profile;
 use Faker\Generator as Faker;
 
@@ -10,7 +9,7 @@ $factory->define(Profile::class, function (Faker $faker) {
     return [
         'user_id' => $faker->unique()->numberBetween($min=1, $max=10000),
         'tweet' => $faker->realText(10),
-        'introduction' => $faker->realText($maxNbChars = 100,$indexSize = 3),
+        'introduction' => $faker->realText($maxNbChars=100, $indexSize=3),
         'hobby' => $faker->text(10),
         'blood_type' => 'B',
         'job' => $faker->jobTitle(),
